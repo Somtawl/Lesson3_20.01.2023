4707b789761a7efe77990cbd0553a7e3b8581645
Somtawl <somtawl@yandex.ru>
2023-01-24 03:02:41 +0300

    3-4 закончили

---
+++ task23/task23.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число не мненьше 1");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 1)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        Console.Write("{0} ",Math.Pow(i,3));
+    }
+}
+else
+{
+    Console.WriteLine("Неверное число");
+}
